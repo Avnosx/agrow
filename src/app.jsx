19ef9eed107374b38a99850e7f5f9cd3add6957c
4n1rudh4 <72496314+4n1rudh4@users.noreply.github.com>
2023-09-09 11:00:14 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,29 @@
 
+import React, { useEffect, useState } from 'react';
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Footer from "./components/footer";
+import {auth} from './firebase';
+function App(){
+    const [username,setUser]=useState("");
+    useEffect(()=>{
+        auth.onAuthStateChanged((user)=>{
+            if(user){
+                setUser(user.displayName)
+            } else{
+                setUser("")
+                console.log(user);
+            }
+        })
+    })
+    var today = new Date();
+    const date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+    
+    return <div className='h-screen bg-grey'>
+        <Header date={date} name={username}/>
+        <Hero/>
+        <Footer/>
+    </div>
+}
+
+export default App;
